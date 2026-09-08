e7ad2947bff129f3dc29bce00c5b8dd7bcfa1de9
sudeates122 <sudeates122@gmail.com>
2026-09-08 19:20:30 +0300

    Delete build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Tüm siteyi tek komutla üretir: node build.sh (veya bash build.sh)
-set -e
-node generate.js
-node build-blog.js
-node build-pages.js
-echo ""
-echo "Site hazır. iller/, blog/, index.html, sitemap.xml ve zorunlu sayfalar güncellendi."
